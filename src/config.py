82d864e5dd4158f1813f4394c2a7bc1c063d776f
shaojieMike <44373239+Kirrito-k423@users.noreply.github.com>
2022-12-24 15:38:04 +0800

    feat✨:  add ass file to rename

--- src/config.py
+++ src/config.py
@@ -2,8 +2,8 @@ import global_variable as glv
 
 glv._init()
 
-glv._set("regex_format", "([^0-9]*)([0-9]*)(.*?)((.mkv)|(.mp4))") # for test
-glv._set("self_regex_format", "([0-9]*)(.*?)((.mkv)|(.mp4))") # for test
+glv._set("regex_format", "([^0-9]*)([0-9]*)(.*?)((.mkv)|(.mp4)|(.ass))") # for test
+glv._set("self_regex_format", "([0-9]*)(.*?)((.mkv)|(.mp4)|(.ass))") # for test
 
 
 
